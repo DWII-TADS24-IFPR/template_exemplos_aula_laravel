e9a2316255f5e8263216a56acec6f5ab21f95bfa
efigenioluiz <hanzou0012@gmail.com>
2025-04-30 10:56:11 -0300

    feat: add bootstrap and exemplos of the blade

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PessoaControllers;
 
-Route::get('/pessoas',[PessoaController::class,'index'])->name('pessoas.index');
-Route::get('/pessoas/create',[PessoaController::class,'create']);
-Route::post('/pessoas',[PessoaController::class,'store'])->name('pessoas.store');
+// Route::get('/pessoas',[PessoaController::class,'index'])->name('pessoas.index');
+// Route::get('/pessoas/create',[PessoaController::class,'create']);
+// Route::post('/pessoas',[PessoaController::class,'store'])->name('pessoas.store');
+
+Route::get('/', function () {
+    return view('home', ['idade' => null,'frutas' => ['banana', 'maçã', 'laranja']]);
+})->name('home');
