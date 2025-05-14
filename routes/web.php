4afdc33e38b694a5649f063cfccf79bb6a2cceb1
efigenioluiz <hanzou0012@gmail.com>
2025-05-14 09:48:40 -0300

    feat: add store file in local using flysystem

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PessoaControllers;
+use App\Http\Controllers\DocumentoController;
 
 // Route::get('/pessoas',[PessoaController::class,'index'])->name('pessoas.index');
 // Route::get('/pessoas/create',[PessoaController::class,'create']);
@@ -12,10 +13,14 @@ use App\Http\Controllers\PessoaControllers;
 //     return view('home', ['idade' => null,'frutas' => ['banana', 'maçã', 'laranja']]);
 // })->name('home');
 
-Route::resource('/niveis', NivelController::class);
+// Route::resource('/niveis', NivelController::class);
 
-Route::get('/relatorios', [RelatorioController::class, 'emitirRelatorio'])->name('relatorio.emitir');
+// Route::get('/relatorios', [RelatorioController::class, 'emitirRelatorio'])->name('relatorio.emitir');
 
-Route::get('/graficos', function (){
-    return view('graficos.index');
-})->name('graficos');
+// Route::get('/graficos', function (){
+//     return view('graficos.index');
+// })->name('graficos');
+
+
+Route::get('/documentos/create',[DocumentoController::class,'create'])->name('documentos.create');
+Route::post('/documentos/create',[DocumentoController::class,'store'])->name('documentos.store');
